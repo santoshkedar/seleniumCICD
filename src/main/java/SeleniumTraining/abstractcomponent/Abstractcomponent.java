98d303603a148d019ffd11235c96287c825fa93e
santoshkedar <santosh.kedar31@gmail.com>
2024-02-27 02:17:19 -0600

    Updated Code

--- src/main/java/SeleniumTraining/abstractcomponent/Abstractcomponent.java
+++ src/main/java/SeleniumTraining/abstractcomponent/Abstractcomponent.java
@@ -35,14 +35,16 @@ public class Abstractcomponent
 			wait.until(ExpectedConditions.visibilityOfElementLocated(Findby));
 		}
 	
-	public void waitForVisibilityOfElement(WebElement ele)
+	public void waitForVisibilityOfElement(WebElement ele) throws InterruptedException
 	{
-		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
-		wait.until(ExpectedConditions.visibilityOf(ele));
+		Thread.sleep(3000);
+		//WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
+		//wait.until(ExpectedConditions.visibilityOf(ele));
 	}
 	
-	public void waitForInvisibilityOfElement(WebElement ele)
+	public void waitForInvisibilityOfElement(WebElement ele) throws InterruptedException
 	{
+		Thread.sleep(3000);
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(15));
 		wait.until(ExpectedConditions.invisibilityOf(ele));
 	}
